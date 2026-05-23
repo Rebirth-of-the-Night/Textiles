3ab5c3ae9d2957dd41d7c61891867860437a6b5d
Eve <phanta@phanta.pl>
2026-05-22 20:05:33 -0500

    Update build system

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  zulu = pkgs.zulu17;
+  zulu8 = pkgs.zulu8;
+
+  gradlePropPrefix = "ORG_GRADLE_PROJECT_";
+
+  runtimeDeps = with pkgs; [ libpulseaudio libGL glfw openal stdenv.cc.cc.lib udev ]
+    ++ (with xorg; [ libX11 libXext libXcursor libXrandr libXxf86vm ]);
+in
+
+pkgs.mkShell {
+  nativeBuildInputs = [ zulu pkgs.python3 ];
+  buildInputs = [ zulu8 ];
+
+  "${gradlePropPrefix}org.gradle.java.installations.auto-detect" = "false";
+  "${gradlePropPrefix}org.gradle.java.installations.auto-download" = "false";
+  "${gradlePropPrefix}org.gradle.java.installations.paths" = "${zulu},${zulu8}";
+
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtimeDeps;
+}
